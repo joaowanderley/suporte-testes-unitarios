385929b95a10447a351113a299c9307ae3583da5
João <joaowannderley@hotmail.com>
2025-05-20 22:21:19 -0300

    configuracao adicionada para poder fazer teste de integracao de ponta a ponta. Do controller a camada de repositório.

---
+++ src/test/java/com/example/demo/integrationtest/PersonControllerIntegrationTest.java
@@ -0,0 +1,43 @@
+package com.example.demo.integrationtest;
+
+import com.example.demo.config.TestConfigs;
+import com.example.demo.integrationtest.testcontainers.AbstractIntegrationTest;
+import com.example.demo.model.Person;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class PersonControllerIntegrationTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+    private static ObjectMapper objectMapper;
+    private static Person person;
+
+    @BeforeAll
+    public static void setup() {
+        // given / arrange
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        specification = new RequestSpecBuilder()
+                .setBasePath("")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+        person = new Person("joao", "paulo", "maceio", "male", "joao@gmail.com");
+    }
+}
